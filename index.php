4ef88edf2eb404844ca290c3c45bf5a110343644
eyasu13 <eyasukibru13@gmail.com>
2022-02-15 20:43:06 +0300

    c

--- index.php
+++ index.php
@@ -14,16 +14,9 @@ session_start();
   
 
     <?php 
-    $user_model= $_SESSION["account-type"];
-     if($user_model == "admin"){
-        include_once('pages/adminheader.php');
-     }
-    else if($user_model == "user"){
-        include_once('pages/userheader.php');       
-    }
-    else{
+    
        include_once('pages/header.php');
-    }
+    
 ?>
     <main>
 
